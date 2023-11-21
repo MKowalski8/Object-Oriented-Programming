3ad93d9bc156a9fdda0e099526f760a20cfbd6d6
MKowalski8 <makow.088@gmail.com>
2023-11-21 10:55:18 +0100

    Improved move test

--- Animals/src/test/java/agh/ics/oop/model/RectangularMapTest.java
+++ Animals/src/test/java/agh/ics/oop/model/RectangularMapTest.java
@@ -77,20 +77,25 @@ class RectangularMapTest {
     void canMoveAnAnimalOnMap() {
         //given
         RectangularMap map = new RectangularMap(4, 4);
+
         Vector2d animal1StartVector = new Vector2d(2, 0);
         Vector2d animal2StartVector = new Vector2d(3, 4);
         Vector2d animal3StartVector = new Vector2d(3, 0);
         Vector2d animal4StartVector = new Vector2d(1, 2);
+        Vector2d animal5StartVector = new Vector2d(0, 0);
+
         Animal animal1 = new Animal(animal1StartVector);
         Animal animal2 = new Animal(animal2StartVector);
         Animal animal3 = new Animal(animal3StartVector);
         Animal animal4 = new Animal(animal4StartVector);
+        Animal animal5 = new Animal(animal5StartVector);
+
         map.place(animal1);
         map.place(animal2);
         map.place(animal3);
         map.place(animal4);
 //        I need this to check if previous positions
-//        after move were deleted
+//        after moves were deleted
         Map<Vector2d, Animal> animals = map.getMap();
 
         //when
@@ -100,7 +105,8 @@ class RectangularMapTest {
         map.move(animal3, MoveDirection.FORWARD);
         map.move(animal4, MoveDirection.RIGHT);
         map.move(animal4, MoveDirection.FORWARD);
-        System.out.println(map);
+        map.move(animal5, MoveDirection.FORWARD);
+//        System.out.println(map);
 
         //then
 //        nothing changes, because it would be out of map
@@ -112,6 +118,10 @@ class RectangularMapTest {
         assertEquals(animal4, animals.get(new Vector2d(2,2)));
 //        check if previous position was deleted
         assertFalse(animals.containsKey(animal4StartVector));
+//        move of animal5 wasn't done, because it wasn't placed,
+//        so check if it isn't on the old place or on the place where it would go
+        assertFalse(animals.containsKey(animal5StartVector));
+        assertFalse(animals.containsKey(new Vector2d(0,1)));
     }
 
 
